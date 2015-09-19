fde1e894b3946e206e3cec623cd5b5b48f13a985
Lau Thiam Kok <lau.thiamkok@yahoo.co.uk>
2015-09-19 13:26:59 +0100

    Added template sample.

--- slim/slim-3/template-twig/public/index.php
+++ slim/slim-3/template-twig/public/index.php
@@ -2,7 +2,34 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\App();
+// @ref: http://www.slimframework.com/docs/features/templates.html
+// Create container
+$container = new \Slim\Container;
+
+// Register component on container
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('template/', [
+        //'cache' => 'path/to/cache'
+    ]);
+
+    // Add extensions
+    $view->addExtension(new Slim\Views\TwigExtension($c->get('router'), $c->get('request')->getUri()));
+    $view->addExtension(new Twig_Extension_Debug());
+
+    return $view;
+};
+
+//Override the default Not Found Handler
+$container['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $c['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write('Page not found');
+    };
+};
+
+$app = new \Slim\App($container);
 
 // Routes:
 $app->get('/hello/{name}', function ($request, $response, $args) {
@@ -10,12 +37,9 @@ $app->get('/hello/{name}', function ($request, $response, $args) {
     // fetch the id attribute to discover what was matched.
     $name = $request->getAttribute('name');
 
-    $status = $response->getStatusCode();
-    //var_dump($status);
-
-    //echo 'Hello, ' . $name;
-    $response->getBody()->write('Hello, ' . $name);
-    return $response->withHeader('Content-type', 'application/json');
+    return $this->view->render($response, 'profile.twig', [
+        'name' => $args['name']
+    ]);
 
 });
 
