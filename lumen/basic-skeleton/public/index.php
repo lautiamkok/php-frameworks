82c8273a9cbc8e08007439b833e090744e399d14
Lau Thiam Kok <lau.thiamkok@yahoo.co.uk>
2015-11-07 03:33:37 +0000

    Renamed folder. Added routes to the basic sample.

--- lumen/basic-skeleton/public/index.php
+++ lumen/basic-skeleton/public/index.php
@@ -25,4 +25,11 @@ $app = require __DIR__.'/../bootstrap/app.php';
 |
 */
 
-$app->run();
+// // @ref: https://laracasts.com/discuss/channels/lumen/always-404-sorry-the-page-you-are-looking-for-could-not-be-found
+// // @ref: http://stackoverflow.com/questions/30891405/not-found-page-in-lumen-after-install
+// $app->run($app['request']);
+
+// Or:
+// @ref: http://stackoverflow.com/questions/29728973/notfoundhttpexception-with-lumen/30254364#30254364
+$request = Illuminate\Http\Request::capture();
+$app->run($request);
---
+++ lumen/basic/public/index.php
@@ -0,0 +1,41 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Laravel\Lumen\Application(
+    realpath(__DIR__.'/../')
+);
+
+// Basic Routing.
+$app->get('/', function () {
+    return 'Hello World';
+});
+
+$app->post('foo/bar', function () {
+    return 'Hello World';
+});
+
+$app->put('foo/bar', function () {
+    //
+});
+
+$app->delete('foo/bar', function () {
+    //
+});
+
+// Route Parameters.
+$app->get('user/{id}', function ($id) {
+    return 'User '.$id;
+});
+
+$app->get('posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    //
+});
+
+// // @ref: https://laracasts.com/discuss/channels/lumen/always-404-sorry-the-page-you-are-looking-for-could-not-be-found
+// // @ref: http://stackoverflow.com/questions/30891405/not-found-page-in-lumen-after-install
+// $app->run($app['request']);
+
+// Or:
+// @ref: http://stackoverflow.com/questions/29728973/notfoundhttpexception-with-lumen/30254364#30254364
+$request = Illuminate\Http\Request::capture();
+$app->run($request);
