16f661f9e67830e4b129835a2c170df31a2b89e8
Lau Thiam Kok <lau.thiamkok@yahoo.co.uk>
2016-03-23 21:15:30 +0800

    Removed unused lines.

--- slim/slim-3/modular-bootstrap-twig-auth/module/config/core/Blog/Article/routes.php
+++ slim/slim-3/modular-bootstrap-twig-auth/module/config/core/Blog/Article/routes.php
@@ -3,27 +3,16 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+// use Spectre\Blog\Article\Controller\BlogArticleController; // to be developed
+
 // API group
 $app->group('/blog', function () {
     // Get the login form.
     $this->get('/{url:[a-zA-Z0-9\-]+}', function (Request $request, Response $response, array $args) {
-        // // Get an instance of the Twig Environment.
-        // $twig = $this->view;
+        // // Get the result via the controller.
+        // $controller = new BlogArticleController($this); to be developed.
+        // $controller($request, $response, $args);
 
-        // // From that get the Twig Loader instance (file loader in this case).
-        // $loader = $twig->getLoader();
-
-        // // Add the module template and additional paths to the existing.
-        // $loader->addPath(APPLICATION_ROOT . 'public/theme/default/');
-        // $loader->addPath(APPLICATION_ROOT . 'public/theme/default/Login/');
-
-        // // Load the template through the Twig service in the DIC.
-        // $template = $twig->loadTemplate('index.twig');
-        // // or:
-        // // $template = $this->getContainer()->get('view')->loadTemplate('Login/index.twig');
-
-        // // Render the template using a simple content variable.
-        // return $response->write($template->render([]));
-        return $response->getBody()->write('Fallback Hello World!');
+        return $response->getBody()->write('Blog article page to be developed');
     });
 });
