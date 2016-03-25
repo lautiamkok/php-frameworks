67852ac620cc4ba70f0ed23ef2bdefbabdaeb4aa
Lau Thiam Kok <lau.thiamkok@yahoo.co.uk>
2016-03-25 17:27:12 +0800

    Removed try catch from the controller.

--- slim/slim-3/modular-bootstrap-twig-auth/module/source/core/Blog/Controller/BlogController.php
+++ slim/slim-3/modular-bootstrap-twig-auth/module/source/core/Blog/Controller/BlogController.php
@@ -37,82 +37,74 @@ class BlogController extends AbstractController
      */
     public function __invoke(Request $request, Response $response, array $args)
     {
-       // Trigger exception in a "try" block
-        try {
-            // Get the core & local database configurations.
-            $databaseCore = require $this->container->settings['database']['core'];
-            $databaseLocal = require $this->container->settings['database']['local'];
+        // Get the core & local database configurations.
+        $databaseCore = require $this->container->settings['database']['core'];
+        $databaseLocal = require $this->container->settings['database']['local'];
 
-            // Merge the configurations.
-            $databaseConfig = array_merge($databaseCore, $databaseLocal);
+        // Merge the configurations.
+        $databaseConfig = array_merge($databaseCore, $databaseLocal);
 
-            // Instance of PdoAdapter.
-            $PdoAdapter = new PdoAdapter($databaseConfig['dsn'], $databaseConfig['username'], $databaseConfig['password']);
+        // Instance of PdoAdapter.
+        $PdoAdapter = new PdoAdapter($databaseConfig['dsn'], $databaseConfig['username'], $databaseConfig['password']);
 
-            // Make connection.
-            $PdoAdapter->connect();
+        // Make connection.
+        $PdoAdapter->connect();
 
-            // Gateway & Mapper.
-            $BlogGateway = new BlogGateway($PdoAdapter);
-            $BlogMapper = new BlogMapper($BlogGateway);
-            $BlogCollectionGateway = new BlogCollectionGateway($PdoAdapter);
-            $BlogCollectionMapper = new BlogCollectionMapper($BlogCollectionGateway);
+        // Gateway & Mapper.
+        $BlogGateway = new BlogGateway($PdoAdapter);
+        $BlogMapper = new BlogMapper($BlogGateway);
+        $BlogCollectionGateway = new BlogCollectionGateway($PdoAdapter);
+        $BlogCollectionMapper = new BlogCollectionMapper($BlogCollectionGateway);
 
-            // Service.
-            $BlogService = new BlogService($BlogMapper, $BlogCollectionMapper);
+        // Service.
+        $BlogService = new BlogService($BlogMapper, $BlogCollectionMapper);
 
-            // Visitors/ Flyweights.
-            $BlogCollectionArticleContentMapper = new BlogCollectionArticleContentMapper(new BlogCollectionArticleContentGateway($PdoAdapter));
+        // Visitors/ Flyweights.
+        $BlogCollectionArticleContentMapper = new BlogCollectionArticleContentMapper(new BlogCollectionArticleContentGateway($PdoAdapter));
 
-            // Inject the visitors/ flyweights.
-            $BlogCollectionMapper->addFlyweight($BlogCollectionArticleContentMapper);
+        // Inject the visitors/ flyweights.
+        $BlogCollectionMapper->addFlyweight($BlogCollectionArticleContentMapper);
 
-            // Get the blog.
-            $BlogModel = $BlogService->getBlog([
-                "url" => 'blog',
-                "collection" => [
-                    "type" => "post",
-                    "start_row" => 0,
-                    "limit" => 6
-                ]
-            ]);
+        // Get the blog.
+        $BlogModel = $BlogService->getBlog([
+            "url" => 'blog',
+            "collection" => [
+                "type" => "post",
+                "start_row" => 0,
+                "limit" => 6
+            ]
+        ]);
 
-            print_r($BlogModel);
+        print_r($BlogModel);
 
-            // // Get format in the query string.
-            // $allGetVars = $request->getQueryParams();
-            // $format = isset($allGetVars['format']) ? $allGetVars['format'] : null;
+        // // Get format in the query string.
+        // $allGetVars = $request->getQueryParams();
+        // $format = isset($allGetVars['format']) ? $allGetVars['format'] : null;
 
-            // // Encode the data to json - if the json is requested.
-            // if ($format === 'json') {
-            //     return $response->getBody()->write(json_encode($Blog));
-            // }
+        // // Encode the data to json - if the json is requested.
+        // if ($format === 'json') {
+        //     return $response->getBody()->write(json_encode($Blog));
+        // }
 
-            // // Get an instance of the Twig Environment.
-            // $twig = $this->container->view;
+        // // Get an instance of the Twig Environment.
+        // $twig = $this->container->view;
 
-            // // From that get the Twig Loader instance (file loader in this case).
-            // $loader = $twig->getLoader();
+        // // From that get the Twig Loader instance (file loader in this case).
+        // $loader = $twig->getLoader();
 
-            // // Add the module template and additional paths to the existing.
-            // $loader->addPath(APPLICATION_ROOT . 'public/theme/default/');
-            // $loader->addPath(APPLICATION_ROOT . 'public/theme/default/Blog/');
+        // // Add the module template and additional paths to the existing.
+        // $loader->addPath(APPLICATION_ROOT . 'public/theme/default/');
+        // $loader->addPath(APPLICATION_ROOT . 'public/theme/default/Blog/');
 
-            // // Load the template through the Twig service in the DIC.
-            // $template = $twig->loadTemplate('index.twig');
+        // // Load the template through the Twig service in the DIC.
+        // $template = $twig->loadTemplate('index.twig');
 
-            // // Render the template using a simple content variable.
-            // return $response->write($template->render([
-            //     'baseUrl' => BASE_URL,
-            //     'blogId' => $BlogModel->getBlogId(),
-            //     'title' => $BlogModel->getTitle(),
-            //     'content' => $BlogModel->getContent()
-            // ]));
-
-        } catch(\Exception $e) {
-            echo 'Message: ' .$e->getMessage();
-            // $container = $this;
-            // return require_once APPLICATION_ROOT . 'module/result/core/PageNotFound/index.php';
-        }
+        // // Render the template using a simple content variable.
+        // return $response->write($template->render([
+        //     'baseUrl' => BASE_URL,
+        //     'blogId' => $BlogModel->getBlogId(),
+        //     'title' => $BlogModel->getTitle(),
+        //     'content' => $BlogModel->getContent()
+        // ]));
     }
 }
